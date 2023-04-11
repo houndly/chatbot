887c25acfc5376006f92f5da7f36b14879466283
Juan <juanpablo.munoz@uber.com>
2023-04-11 12:38:35 -0500

    first script to test chatbot

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from flask_ngrok import run_with_ngrok
 
 
 app = Flask(__name__)
+run_with_ngrok(app)
 
 
 @app.route("/sms", methods=["POST"])
@@ -10,7 +12,17 @@ def reply():
     incoming_msg = request.form.get("Body").lower()
     response = MessagingResponse()
     message = response.message()
+    responded = False
+    words = incoming_msg.split("")
+    if "hola" in words:
+        message.body("Hola, soy un bot de prueba")
+        responded = True
+
+    if not responded:
+        message.body("No se que decir")
+
+    return str(response)
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
