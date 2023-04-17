dc68ce74b02cb3fdcd76c1ce77a8a5b13464ad01
HajdukSanchez <hajduksanchez.dev@gmail.com>
2023-04-16 23:17:07 -0500

    🚧 [ADD] Appointment entity

--- app/appointment/__init__.py
+++ app/appointment/__init__.py
@@ -1,10 +1,13 @@
+from app.appointment.appointment import Appointment
 from app.common.sheets import getConnection as sheets
 
-def register_appointment() -> bool:
+def register_appointment(appointment: Appointment) -> bool:
 	"""
  	Registers an appointment with the veterinarian
   """
 	# Set row values to insert
-	new_row = [] # TODO: ADD Class to handle values to insert into row
-	registration = sheets.insert_data(new_row) # Set sheet connection
-	return registration # New appointment register successfully
+	new_row = appointment.get_data_to_row()
+
+	is_register = sheets.insert_data(new_row)
+
+	return is_register # New appointment register successfully
